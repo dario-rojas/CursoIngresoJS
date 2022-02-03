0d8bbaf5552de6da2b9603a1061c317af839a1a3
unknown <dariorojas88@outloock.com.ar>
2022-02-03 16:24:40 -0300

    ejercicios

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,4 +1,4 @@
-/*
+/* dario daniel rojas entrega ejercicio 1 de entrada y salida, 
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,16 @@
-/*
+/* dario daniel rojas ejercicio 2 entrada y salida.
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+
 function mostrar()
-{
-	alert("ok");
+
+{/*para declarar una variable ( cajita) se usa la palabra "let"*/
+	let nombre;
+
+	/*esto es una asignacion "=" */
+	/*el prompt me permite pedir datos por teclado*/
+    nombre = prompt("ingrese su nombre")
+    /*estoy mostrando un cartelito con "alert"*/
+	alert(nombre);
 }
 
